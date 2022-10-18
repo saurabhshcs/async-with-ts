089759aec0096a47e11944f95bcfa9436d49eb41
saurabhshcs <saurabhshcs@gmail.com>
2022-10-18 02:21:00 +0100

    Initial commit for the testing of the React node fetch api

--- index.tsx
+++ index.tsx
@@ -1,5 +1,6 @@
 import fetch from "node-fetch";
 
+//This API is gonna test pokemon api for learning of the async method and promise concept
 fetch("")
 .then((res) => res.json)
 .then((data) => console.log(data));
